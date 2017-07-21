234ce4e7b752dfca64bf95041165766be3414ec4
仲达超 <sun0821vip@163.com>
2017-07-21 16:38:21 +0800

    add simple2

--- player_simple2/player_simple2.h
+++ player_simple2/player_simple2.h
@@ -2,21 +2,19 @@
 
 #include <stdio.h>
 #include <tchar.h>
-#include <io.h>
-#include <direct.h>
+
 extern "C"
 {
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
-// 新版里的图像转换结构需要引入的头文件;
 #include "libswscale/swscale.h"
-// SDL1.2
-#include "sdl/SDL.h"
-#include "sdl/SDL_thread.h"
+// SDL2.0
+#include "sdl2/SDL.h"
+#include "sdl2/SDL_thread.h"
 
 #pragma comment(lib,"avcodec.lib")
 #pragma comment(lib,"avformat.lib")
 #pragma comment(lib,"avutil.lib")
 #pragma comment(lib,"swscale.lib")
-#pragma comment(lib,"SDL.lib")
+#pragma comment(lib,"SDL2.lib")
 };
